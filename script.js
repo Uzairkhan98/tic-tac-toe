c412b74c77294e234e9c8b84161380c23e322a23
Muhammad Uzair Khan <uzair_khan2012@hotmail.com>
2022-09-05 13:43:46 +0500

    Added module for changing game state

--- script.js
+++ script.js
@@ -4,3 +4,29 @@
         document.getElementById(`player${index}`).focus();
     })
 });
+
+let displayController = (function(){
+    let _gameState = false
+    function changeState(){
+        _gameState = !_gameState
+        console.log(_gameState)
+        let header = document.getElementById('header')
+        let gameButton = document.getElementById('gameButton')
+        if(_gameState){
+            header.setAttribute('style', 'visibility:hidden')
+            gameButton.innerText = "Reset Game"
+        }
+        else{
+            header.setAttribute('style', 'visibility:visible')
+            gameButton.innerText = "Start Game"
+        }
+    }
+    return {
+        changeState
+    }
+})()
+
+function changeGameState(){
+    displayController.changeState()
+}
+
